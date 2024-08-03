d64bd18b61fc5c59b51e1e3ef0e24990e964b3b0
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-08-03 10:39:18 +0500

    8c updated

--- 8c.js
+++ 8c.js
@@ -1,9 +1,9 @@
 'use strict'
 const productsOfAmazon={
-    nameOfProduct:'basketball',
-    priceOfProduct: 2095
+    nameOfProduct:'gandboll',
+    priceOfProduct: 3000
 }
-productsOfAmazon.priceOfProduct=productsOfAmazon.priceOfProduct + 500
+productsOfAmazon.priceOfProduct += 500
 console.log(productsOfAmazon)
 productsOfAmazon['deliveryTime'] = "3 days"
 console.log(productsOfAmazon)
