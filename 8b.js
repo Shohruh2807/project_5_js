e290036da484cc8982e16e6a98606f17bdf31d8d
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-08-03 10:39:10 +0500

    8b updated

--- 8b.js
+++ 8b.js
@@ -3,5 +3,5 @@ const productsOfAmazon={
     nameOfProduct:'gandboll',
     priceOfProduct: 3000
 }
-productsOfAmazon.priceOfProduct=productsOfAmazon.priceOfProduct + 500
+productsOfAmazon.priceOfProduct += 500
 console.log(productsOfAmazon)
